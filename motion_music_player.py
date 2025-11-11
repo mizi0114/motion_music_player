1c49ea12fd8128c076a0a6dbfce5b1ac71a5ebdb
mizi0114 <130463435+mizi0114@users.noreply.github.com>
2025-11-11 09:15:19 +0000

    Refactor audio playback to use pydub
    Updated the audio playback method to use pydub for MP3 files and improved motion detection handling.

--- motion_music_player.py
+++ motion_music_player.py
@@ -1,30 +1,35 @@
 import RPi.GPIO as GPIO
-import pygame
+from pydub import AudioSegment
+from pydub.playback import play
 from time import sleep
 import random
 
-GPIO.setmode(GPIO.BCM)
 
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
 
-pygame.mixer.init()
 
-sound = ["/audio/filepath",
-         "/audio/filepath",
-         "/audio/filepath",
-         "/audio/filepath"
-         ]
+#sample clips
+sound = [
+		AudioSegment.from_file("/audio/filepath", format="mp3"),
+		AudioSegment.from_file("/audio/filepath", format="mp3"),	
+		AudioSegment.from_file("/audio/filepath", format="mp3"),
+		AudioSegment.from_file("/audio/filepath", format="mp3"),
+		AudioSegment.from_file("/audio/filepath", format="mp3")				
+		]
 
 
 # motion detection 
-while True:
-    if GPIO.input(23):
-        print("Detected!")
-        audio = pygame.mixer.Sound(sound[random.randint(0, 3)])
-        playing = audio.play()
-        while playing.get_busy():
-            pygame.time.delay(400)
-        sleep(1)
-    else:
-        print("No Motion")
-        sleep(1)
+try:
+	while True:
+		if GPIO.input(23):
+			print("Detected!")
+			play(random.choice(sound))
+			sleep(1)
+		else:
+			print("No Motion")
+			sleep(1)
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
+	pydub.quit()
